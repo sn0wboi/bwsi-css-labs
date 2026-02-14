47d0668b4daaa3b197f6b370639c68399696fc2c
Aiden Xue <aiden.b.xue@gmail.com>
2026-02-14 15:59:19 -0500

    added personal description to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Aiden Xue" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi everybody! My name is Aiden, and I am a sophomore from Massachusetts. A fun fact about me is that I am a captain for my school's rocketry team, which participates in the American Rocketry Challenge. I am looking forward to keep learning with everybody!");
 if __name__ == "__main__":
     main()
