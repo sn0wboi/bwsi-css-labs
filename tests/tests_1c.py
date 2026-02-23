3898f00d24b556cbfb0aae5f6d71047af705d85e
Aiden Xue <aiden.b.xue@gmail.com>
2026-02-23 14:37:44 -0500

    created unit tests for max subarray sum

---
+++ tests/tests_1c.py
@@ -0,0 +1,16 @@
+"""
+tests_1c.py
+
+This module contains unit tests for the max_subarray_sum function defined in lab_1c.py.
+"""
+
+import pytest
+from labs.lab_1.lab_1c import max_subarray_sum
+def test():
+    assert max_subarray_sum([-2,1,-3,4,-1,2,1,-5,4]) == 6 ## test regular case
+    assert max_subarray_sum([-2,1,-3,-4,-1,-2,-1,-5,-4]) == -1 #test when all nums are negative
+    assert max_subarray_sum([0, 0, 0, 0, 0, 0]) == 0 # test when all values are zero
+
+
+if __name__ == "__main__":
+    pytest.main()
